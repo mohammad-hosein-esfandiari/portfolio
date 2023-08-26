0b21ab30068f34185293001ee2d22e8b168d870c
mohammadhosseinesfandiari <mohammadhosseinesfandiari1@gmail.com>
2023-08-26 20:50:13 +0330

    set bollet list for project text

--- src/Components/sections/Projects/Description.jsx
+++ src/Components/sections/Projects/Description.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 export const Description = ({ description }) => {
   return (
-    <div className=" mt-2">
-      {description.map((text, index) => (
-        <p key={index} className="mb-1 text-[14px] px-2 text-gray-p">
-          {text}
-        </p>
-      ))}
+    <div className=" mt-2 text-[14px] leading-[17px] px-2 text-gray-p">
+      <ul className="list-disc pl-1">
+        {description.map((text, index) => (
+          <li key={index} className="mb-1 text-[14px] px-2 text-gray-p">
+            {text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
